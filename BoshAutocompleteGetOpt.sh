767531c00c34d0310969488d7c95be3fe35090ae
Danny Berger <dberger@pivotal.io>
2015-12-18 10:55:02 -0800

    Example command-specific autocomplete for options
    Signed-off-by: Brian Cunnie <bcunnie@pivotal.io>

--- BoshAutocompleteGetOpt.sh
+++ BoshAutocompleteGetOpt.sh
@@ -10,14 +10,14 @@ _BoshComplete ()   #  By convention, the function name
   COMPREPLY=()   # Array variable storing the possible completions.
   cur=${COMP_WORDS[COMP_CWORD]}
 
-  case "$cur" in
+  case "${COMP_WORDS[1]}" in
+    vms)
+      COMPREPLY=( $( compgen -W '--details --dns --vitals' -- $cur ) )
+      ;;
     *)
       # top level commands
-    COMPREPLY=( $( compgen -W 'add alias aliases aws backup blobs cancel cleanup cloud-config cloudcheck complete create delete deploy deployment deployments diff disks download edit errands export finalize generate get help init inspect instances locks login logout logs micro properties public recreate releases rename reset restart run destination_directory] scp set snapshots ssh start status stemcells stop sync take target targets task tasks unset update upload validate verify version vm vms' -- $cur ) );;
-#   Generate the completion matches and load them into $COMPREPLY array.
-#   xx) May add more cases here.
-#   yy)
-#   zz)
+      COMPREPLY=( $( compgen -W 'add alias aliases aws backup blobs cancel cleanup cloud-config cloudcheck complete create delete deploy deployment deployments diff disks download edit errands export finalize generate get help init inspect instances locks login logout logs micro properties public recreate releases rename reset restart run destination_directory] scp set snapshots ssh start status stemcells stop sync take target targets task tasks unset update upload validate verify version vm vms' -- $cur ) )
+      ;;
   esac
 
   return 0
